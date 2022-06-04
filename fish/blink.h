7d400e6167cc8ced2881b930e03b3db7dc021ab9
Amadeus Justinn Haryanto <amadeusjustinn@gmail.com>
2022-06-04 12:30:17 +0700

    Formatted fish files

--- fish/blink.h
+++ fish/blink.h
@@ -3,13 +3,14 @@
 #define RTC_FIND 2
 #define RTC_SYNC 3
 
-struct mp1_blink_struct {
+struct mp1_blink_struct
+{
   unsigned short location;
-  char on_char; 
+  char on_char;
   char off_char;
   unsigned short on_length;
   unsigned short off_length;
   unsigned short countdown;
   unsigned short status;
-  struct mp1_blink_struct* next;
-} __attribute((packed)); 
+  struct mp1_blink_struct *next;
+} __attribute((packed));
--- fish/fish.c
+++ fish/fish.c
@@ -6,10 +6,10 @@
 #define NULL 0
 #define WAIT 100
 uint8_t *vmem_base_addr;
-uint8_t *mp1_set_video_mode (void);
+uint8_t *mp1_set_video_mode(void);
 void add_frames(uint8_t *, uint8_t *, int32_t);
-void ece391_memset(void* memory, char c, int n);
-int32_t ece391_memcpy(void* dest, const void* src, int32_t n);
+void ece391_memset(void *memory, char c, int n);
+int32_t ece391_memcpy(void *dest, const void *src, int32_t n);
 
 uint8_t file0[] = "frame0.txt";
 uint8_t file1[] = "frame1.txt";
@@ -18,27 +18,29 @@ uint8_t file1[] = "frame1.txt";
 extern int mp1_ioctl(unsigned long arg, unsigned long cmd);
 extern void mp1_rtc_tasklet(unsigned long trash);
 
-static struct mp1_blink_struct blink_array[80*25];
+static struct mp1_blink_struct blink_array[80 * 25];
 
 int main(void)
 {
     int rtc_fd, ret_val, i, garbage;
     struct mp1_blink_struct blink_struct;
 
-    ece391_memset(blink_array, 0, sizeof(struct mp1_blink_struct)*80*25);
+    ece391_memset(blink_array, 0, sizeof(struct mp1_blink_struct) * 80 * 25);
 
-    if(mp1_set_video_mode() == NULL) {
+    if (mp1_set_video_mode() == NULL)
+    {
         return -1;
     }
 
-    rtc_fd = ece391_open((uint8_t*)"rtc");
+    rtc_fd = ece391_open((uint8_t *)"rtc");
 
     add_frames(file0, file1, rtc_fd);
 
     ret_val = 32;
     ret_val = ece391_write(rtc_fd, &ret_val, 4);
 
-    for(i=0; i<WAIT; i++) {
+    for (i = 0; i < WAIT; i++)
+    {
         ece391_read(rtc_fd, &garbage, 4);
         mp1_rtc_tasklet(garbage);
     }
@@ -47,25 +49,28 @@ int main(void)
     blink_struct.off_char = 'M';
     blink_struct.on_length = 7;
     blink_struct.off_length = 6;
-    blink_struct.location = 6*80+60;
+    blink_struct.location = 6 * 80 + 60;
 
     mp1_ioctl((unsigned long)&blink_struct, RTC_ADD);
 
-    for(i=0; i<WAIT; i++) {
+    for (i = 0; i < WAIT; i++)
+    {
         ece391_read(rtc_fd, &garbage, 4);
         mp1_rtc_tasklet(garbage);
     }
 
-    mp1_ioctl((40 << 16 | (6*80+60)), RTC_SYNC);
+    mp1_ioctl((40 << 16 | (6 * 80 + 60)), RTC_SYNC);
 
-    for(i=0; i<WAIT; i++) {
+    for (i = 0; i < WAIT; i++)
+    {
         ece391_read(rtc_fd, &garbage, 4);
         mp1_rtc_tasklet(garbage);
     }
 
-    mp1_ioctl(6*80+60, RTC_REMOVE);
+    mp1_ioctl(6 * 80 + 60, RTC_REMOVE);
 
-    for(i=0; i<WAIT; i++) {
+    for (i = 0; i < WAIT; i++)
+    {
         ece391_read(rtc_fd, &garbage, 4);
         mp1_rtc_tasklet(garbage);
     }
@@ -75,8 +80,7 @@ int main(void)
     return 0;
 }
 
-void
-add_frames(uint8_t *f0, uint8_t *f1, int32_t rtc_fd)
+void add_frames(uint8_t *f0, uint8_t *f1, int32_t rtc_fd)
 {
     int32_t row, col, offset = 40, eof0 = 0, eof1 = 0, num_bytes;
     int32_t fd0, fd1;
@@ -88,58 +92,75 @@ add_frames(uint8_t *f0, uint8_t *f1, int32_t rtc_fd)
 
     row = 0;
 
-    if( (fd0 = ece391_open(f0)) < 0 ) {
+    if ((fd0 = ece391_open(f0)) < 0)
+    {
         ece391_halt(-1);
     }
-    if( (fd1 = ece391_open(f1)) < 0 ) {
+    if ((fd1 = ece391_open(f1)) < 0)
+    {
         ece391_halt(-1);
     }
 
-    while(eof0 == 0 || eof1 == 0) {
+    while (eof0 == 0 || eof1 == 0)
+    {
         col = 0;
-        while(1) {
+        while (1)
+        {
 
-            if(c0 != '\n') {
+            if (c0 != '\n')
+            {
                 num_bytes = ece391_read(fd0, &c0, 1);
-                if(num_bytes == 0) {
+                if (num_bytes == 0)
+                {
                     c0 = '\n';
                     eof0 = 1;
                 }
             }
 
-            if(c1 != '\n') {
+            if (c1 != '\n')
+            {
                 num_bytes = ece391_read(fd1, &c1, 1);
-                if(num_bytes == 0) {
+                if (num_bytes == 0)
+                {
                     c1 = '\n';
                     eof1 = 1;
                 }
             }
 
-            if(c0 == '\n' && c1 == '\n') {
+            if (c0 == '\n' && c1 == '\n')
+            {
                 break;
-
-            } else {
-                if((c0 != ' ' && c0 != '\n') || (c1 != ' ' && c1 != '\n')) {
-                    blink_struct.on_char = ( (c0 == '\n') ? ' ' : c0);
-                    blink_struct.off_char = ( (c1 == '\n') ? ' ' : c1);
-                    blink_struct.location = row*80 + col + offset;
+            }
+            else
+            {
+                if ((c0 != ' ' && c0 != '\n') || (c1 != ' ' && c1 != '\n'))
+                {
+                    blink_struct.on_char = ((c0 == '\n') ? ' ' : c0);
+                    blink_struct.off_char = ((c1 == '\n') ? ' ' : c1);
+                    blink_struct.location = row * 80 + col + offset;
                     mp1_ioctl((unsigned long)&blink_struct, RTC_ADD);
                 }
             }
             col++;
         }
 
-        if(eof0) {
+        if (eof0)
+        {
             c0 = '\n';
             ece391_close(fd0);
-        } else {
+        }
+        else
+        {
             c0 = '0';
         }
 
-        if(eof1) {
+        if (eof1)
+        {
             c1 = '\n';
             ece391_close(fd1);
-        } else {
+        }
+        else
+        {
             c1 = '0';
         }
 
@@ -147,21 +168,26 @@ add_frames(uint8_t *f0, uint8_t *f1, int32_t rtc_fd)
     }
 }
 
-uint8_t*
-mp1_set_video_mode (void)
+uint8_t *
+mp1_set_video_mode(void)
 {
-    if(ece391_vidmap(&vmem_base_addr) == -1) {
+    if (ece391_vidmap(&vmem_base_addr) == -1)
+    {
         return NULL;
-    } else {
+    }
+    else
+    {
         return vmem_base_addr;
     }
 }
 
-void* mp1_malloc(int32_t size)
+void *mp1_malloc(int32_t size)
 {
     int32_t i;
-    for(i=0; i< 80*25; i++) {
-        if(blink_array[i].location == 0) {
+    for (i = 0; i < 80 * 25; i++)
+    {
+        if (blink_array[i].location == 0)
+        {
             return &blink_array[i];
         }
     }
@@ -169,26 +195,28 @@ void* mp1_malloc(int32_t size)
     return NULL;
 }
 
-void mp1_free(void* memory)
+void mp1_free(void *memory)
 {
     ece391_memset(memory, 0, sizeof(struct mp1_blink_struct));
 }
 
-void ece391_memset(void* memory, char c, int n)
+void ece391_memset(void *memory, char c, int n)
 {
-    char* mem = (char*)memory;
+    char *mem = (char *)memory;
     int i;
-    for(i=0; i<n; i++) {
+    for (i = 0; i < n; i++)
+    {
         mem[i] = c;
     }
 }
 
-int32_t ece391_memcpy(void* dest, const void* src, int32_t n)
+int32_t ece391_memcpy(void *dest, const void *src, int32_t n)
 {
     int32_t i;
-    char* d = (char*)dest;
-    char* s = (char*)src;
-    for(i=0; i<n; i++) {
+    char *d = (char *)dest;
+    char *s = (char *)src;
+    for (i = 0; i < n; i++)
+    {
         d[i] = s[i];
     }
 
